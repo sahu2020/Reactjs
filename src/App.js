c9801be225a2318356e30f2a74d61c2ddc5b7244
sramakrishna <sahana.ramakrshna@amadeus.com>
2020-10-05 22:55:10 +0530

    update app with provider

--- src/App.js
+++ src/App.js
@@ -14,12 +14,28 @@ import {
   signInWithGoogle,
 } from "./firebase/firebase.utils";
 import NavigrationBar from './components/navbar/navbar';
+import { Provider } from "react-redux";
+import { Route, Router } from "react-router-dom";
+import MyCard from "./components/cards/cards";
 
 function App() {
 
   
   return (
-   <NavigrationBar/>
+    <div className ='app-container'>
+     <Provider store={store}>
+      <Router>
+    <NavigrationBar/>
+    <Switch>
+      <Route path={'/'} exact component={Items}/>
+
+      
+      <Route path={'/cart'} exact component={MyCart}/>
+    </Switch>
+    <Footer></Footer>
+      </Router>
+     </Provider>
+   </div>
   );
 }
 
